9852ad0803d3fb6229ac39d26b8d8675a185d329
carlknutson <21251348+carlknutson@users.noreply.github.com>
2025-07-14 21:48:20 -0500

    handle custom subdomain deployment

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Set base for subdomain deployment (nationalsites.carlknutson.com)
 export default defineConfig({
   plugins: [react()],
+  base: '/', // For root of custom domain
   server: {
     open: true,
   },
